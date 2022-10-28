71abd7794554a112217e196a038abe5bd4683e06
Ezra Lim <skywno@gmail.com>
2022-10-28 13:17:33 +0300

    Modify #1 - InitializeConfig refactoring

--- src/main/java/me/ezra/security/config/InitializeDefaultConfig.java
+++ src/main/java/me/ezra/security/config/InitializeDefaultConfig.java
@@ -15,8 +15,8 @@ import javax.annotation.PostConstruct;
  */
 @Configuration
 @RequiredArgsConstructor
-@Profile(value = "!test")
-public class InitializeConfig {
+@Profile(value = "!test") // test 에서는 제외
+public class InitializeDefaultConfig {
 
     private final UserService userService;
     private final NoteService noteService;
@@ -28,15 +28,20 @@ public class InitializeConfig {
      * <h2>게시글 등록 4개</h2>
      * <h2>공지사항 등록 2개</h2>
      */
+
     @PostConstruct
-    public void adminAccount() {
+    public void initializeDefaultUser(){
         User user = userService.signup("user", "user");
-        userService.signupAdmin("admin", "admin");
         noteService.saveNote(user, "테스트", "테스트입니다.");
         noteService.saveNote(user, "테스트2", "테스트2입니다.");
         noteService.saveNote(user, "테스트3", "테스트3입니다.");
         noteService.saveNote(user, "여름 여행계획", "여름 여행계획 작성중...");
-        noticeService.saveNotice("환영합니다.", "환영합니다 여러분");
+    }
+    @PostConstruct
+    public void initializeDefaultAdmin() {
+        userService.signupAdmin("admin", "admin");
         noticeService.saveNotice("게시글 작성 방법 공지", "1. 회원가입\n2. 로그인\n3. 게시글 작성\n4. 저장\n* 본인 외에는 게시글을 볼 수 없습니다.");
+        noticeService.saveNotice("환영합니다.", "환영합니다 여러분");
+
     }
 }
